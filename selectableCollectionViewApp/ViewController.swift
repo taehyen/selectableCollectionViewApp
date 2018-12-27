999f59b8772267675249f962a027f432afdf38ad
Taehyen <appletaehyen@gmail.com>
2018-12-28 08:17:24 +0900

    init

---
+++ selectableCollectionViewApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  selectableCollectionViewApp
+//
+//  Created by innergraph on 28/12/2018.
+//  Copyright © 2018 taehyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+
+}
+
